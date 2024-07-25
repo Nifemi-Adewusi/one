26c681c1f73f58dc89aa57c8475100794a0dd0ec
Nifemi Adewusi <nifemiadewusi@gmail.com>
2024-07-25 08:55:54 +0100

    Add Win Message

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 import pickle
-cs = socket.socket(family= socket.AF_INET, type= socket.SOCK_STREAM)
 
-print("Server Started...")
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+print("Client Started...")
 
 IP_ADDRESS = "127.0.0.1"
-
 PORT = 9000
 
 cs.connect((IP_ADDRESS, PORT))
@@ -13,27 +13,38 @@ cs.connect((IP_ADDRESS, PORT))
 msg = input("Message To Server: ")
 cs.send(msg.encode("ascii"))
 
-data = cs.recv(1024)
+data = cs.recv(1024).decode()
+print("Message From Server: ", data)
 
-print("Message From Server ", data)
 data = cs.recv(1024)
 data_array = pickle.loads(data)
 print(data_array)
 
 index = input("Enter index bw 0-3: ")
 while index.lower().strip() != "bye":
- cs.send(index.encode("ascii"))
- value = input("Enter Value Between 1 -9: ")
- cs.send(value.encode("ascii"))
- data = cs.recv(1024)
- if not data:
-  break
- data_array = pickle.loads(data)
- print(f"Array Game After Client Deduction: {data_array}")
+    cs.send(index.encode("ascii"))
+    value = input("Enter Value Between 1 -9: ")
+    cs.send(value.encode("ascii"))
 
- data = cs.recv(1024)
- data_array = pickle.loads(data)
- print(f"Array Game After Server Deduction: {data_array}")
- index = input("Enter index bw 0-3 or type bye to end: ")
+    data = cs.recv(1024)
+    try:
+        winner_message = data.decode()
+        print(winner_message)
+        break
+    except UnicodeDecodeError:
+        data_array = pickle.loads(data)
+        print(f"Array Game After Client Deduction: {data_array}")
 
+    data = cs.recv(1024)
+    try:
+        winner_message = data.decode()
+        print(winner_message)
+        break
+    except UnicodeDecodeError:
+        data_array = pickle.loads(data)
+        print(f"Array Game After Server Deduction: {data_array}")
 
+    index = input("Enter index bw 0-3 or type bye to end: ")
+
+cs.close()
+print("Client Closed...")
--- server.py
+++ server.py
@@ -3,47 +3,43 @@ import random
 import pickle
 
 N = 4
-
 M = 9
-
 arr = []
 
-# def allZeros(arr)
 def checkWinner(arr):
     for num in arr:
         if num != 0:
             return False
     return True
 
-
 def generate(num):
     r = random.randint(0, num)
     return r
+
 def generateArray():
     for i in range(N):
         r = random.randint(1, M)
         arr.append(r)
 
 def pop(arr, N, indexClient, valueClient):
-    if indexClient < 0 or indexClient > N:
+    if indexClient < 0 or indexClient >= N:
         print("Index Error")
-    if valueClient > arr[indexClient] or valueClient < 0:
-        print("It Is Impossible to subtract less value ")
+    elif valueClient > arr[indexClient] or valueClient < 0:
+        print("It Is Impossible to subtract less value")
     else:
-        arr[indexClient] = arr[indexClient] - valueClient
-ss = socket.socket(family= socket.AF_INET, type= socket.SOCK_STREAM)
+        arr[indexClient] -= valueClient
+
+ss = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
 print("Server Started...")
 
 IP_ADDRESS = "127.0.0.1"
-
 PORT = 9000
 
 ss.bind((IP_ADDRESS, PORT))
-
 ss.listen(5)
 
-connection, address  = ss.accept()
+connection, address = ss.accept()
 print(f"Client Connected With Address {address[0]} And Port Number {address[1]}")
 
 data = connection.recv(1024).decode()
@@ -56,33 +52,38 @@ generateArray()
 print(arr)
 data_array = pickle.dumps(arr)
 connection.send(data_array)
+
 while True:
- indexClient = connection.recv(1024).decode()
- indexClient = int(indexClient)
- valueClient = connection.recv(1024).decode()
- valueClient = int(valueClient)
+    indexClient = connection.recv(1024).decode()
+    indexClient = int(indexClient)
+    valueClient = connection.recv(1024).decode()
+    valueClient = int(valueClient)
 
- indexServer = generate(3)
- valueServer = generate(4)
+    indexServer = generate(3)
+    valueServer = generate(4)
 
-# print(indexServer)
+    print(f"Index Client {indexClient} and Value Client {valueClient}")
+    print(f"Index Server {indexServer} and Value Server {valueServer}")
 
- print(f"Index Client {indexClient} and Value Client {valueClient}")
- print(f"Index Server {indexServer} and Value Server {valueServer}")
+    pop(arr, N, indexClient, valueClient)
+    winner = checkWinner(arr)
+    if winner:
+        print("Client Wins")
+        connection.send("Client Wins".encode("ascii"))
+        break
+    print("Array Game After Deduction Of Client: ", arr)
+    data_arr = pickle.dumps(arr)
+    connection.send(data_arr)
 
- pop(arr, N, indexClient, valueClient)
- winner = checkWinner(arr)
- if winner == True:
-     print("Client Wins")
-     break
- print("Array Game After Deduction Of Client: ", arr)
- data_arr = pickle.dumps(arr)
- connection.send(data_arr)
- pop(arr, N, indexServer, valueServer)
- winner = checkWinner(arr)
- if winner == True:
-     print("Server Wins")
-     break
- print("Array Game After Deduction Of Server: ", arr)
- data_arr = pickle.dumps(arr)
- connection.send(data_arr)
+    pop(arr, N, indexServer, valueServer)
+    winner = checkWinner(arr)
+    if winner:
+        print("Server Wins")
+        connection.send("Server Wins".encode("ascii"))
+        break
+    print("Array Game After Deduction Of Server: ", arr)
+    data_arr = pickle.dumps(arr)
+    connection.send(data_arr)
+
+connection.close()
+print("Server Closed...")
